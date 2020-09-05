c507d556f712d6a7d90a114e944fb4e9f4c78f34
Retroflux <9056446+Retroflux@users.noreply.github.com>
2020-09-05 00:48:37 -0400

    Testing Pycharm integration

--- classes/Episode.py
+++ classes/Episode.py
@@ -1,1 +1,17 @@
-#Episode = full round of games from start to finish
+# Episode = full round of games from start to finish
+
+# Number of rounds
+# Episode Number
+# Number of Team Games
+# Number of Solo Games
+# Final Placement Score
+
+class Episode():
+    """docstring for Episode"""
+
+    def __init__(self, numberOfRounds, episodeNumber, numberOfTeamGames, numberOfSoloGames, finalPlayerScore):
+        self._numberOfRounds = numberOfRounds
+        self._episodeNumber = episodeNumber
+        self._numberOfTeamGames = numberOfTeamGames
+        self._numberOfSoloGames = numberOfSoloGames
+        self._finalPlayerScore = finalPlayerScore
--- classes/Round.py
+++ classes/Round.py
@@ -11,7 +11,53 @@ class Round:
 		self._episodeNumber = episodeNumber
 		self._playerScore = playerScore
 
+	@property
+	def name(self):
+		return self._name
 	
+	@property
+	def startingParticipants(self):
+		return self._startingParticipants
+	
+	@property
+	def qualifyingParticipants(self):
+		return self._qualifyingParticipants
+	
+	@property
+	def roundNumber(self):
+		return self._roundNumber
+	
+	@property
+	def episodeNumber(self):
+		return self._episodeNumber
+	
+	@property
+	def playerScore(self):
+		return self._playerScore
+	
+	@name.setter
+	def name(self,name):
+		self._name = name
+
+	@startingParticipants.setter
+	def startingParticipants(self,startingParticipants):
+		self._startingParticipants=startingParticipants
+
+	@qualifyingParrticipants.setter
+	def qualifyingParticipants(self,qualifyingParticipants):
+		self._qualifyingParticipants = qualifyingParticipants
+
+	@roundNumber.setter
+	def roundNumber(self,roundNumber):
+		self._roundNumber = roundNumber
+
+	@episodeNumber.setter
+	def episodeNumber(self,episodeNumber):
+		self._episodeNubmer = episodeNumber
+
+	@playerScore.setter
+	def playerScore(self,playerScore):
+		self._playerScore = playerScore
 
 
 # Name
